2e8b56b31e175513a9c5d7356aa43d7364250595
jya1103 <47504382+jya1103@users.noreply.github.com>
2019-02-23 21:07:16 +0100

    V3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 //https://www.infoworld.com/article/3185447/c-sharp/how-to-work-with-filesystemwatcher-in-c.html
+//https://www.geeksforgeeks.org/c-sharp-arraylist-class/
 namespace MonitorIntelFolderActivity
 {
     class Program
@@ -14,18 +15,22 @@ namespace MonitorIntelFolderActivity
        
         static void Main(string[] args)
         {
-           
-           /* une seule instance
-           string str_dirpath = @"C:\Intel\Logs"; 
-            //C# program that uses List, foreach-loop
-           List<string> list = new List<string>();
-           list.Add(str_dirpath);
-           foreach (string prime in list)  // Loop through List with foreach.
-           {System.Console.WriteLine(prime);
-           }
-            //The directory path is passed as an argument to the method.
-            //  MonitorDirectory(str_dirpath); 
-            */ 
+
+            /* une seule instance
+            string str_dirpath = @"C:\Intel\Logs"; 
+             //C# program that uses List, foreach-loop
+            List<string> list = new List<string>();
+            list.Add(str_dirpath);
+            foreach (string prime in list)  // Loop through List with foreach.
+            {System.Console.WriteLine(prime);
+            }
+             //The directory path is passed as an argument to the method.
+             //  MonitorDirectory(str_dirpath); 
+             */
+
+         
+
+
             MonitorDirectory();
             Console.ReadKey();
         }
@@ -36,33 +41,53 @@ namespace MonitorIntelFolderActivity
         //System.IO will be used for FileSystemWatcher and Directory.
         //System.Collections for the ArrayList
         {
+
             List<string> lstFolders = new List<string>();   //C# program that uses List, foreach-loop
-            ArrayList aFileWatcherInstance = new ArrayList();  //use System.Collections for <ArrayList>;
             lstFolders.Add(@"C:\Intel\Logs");
             lstFolders.Add(@"C:\Intel\");
 
+            ArrayList aFileWatcherInstance = new ArrayList();  //use System.Collections for <ArrayList>;
+            System.Console.WriteLine("..");
             foreach (string sMonitorFolder in lstFolders)
             {
+               
 
                 //Only if Directory Exists
                 if (Directory.Exists(sMonitorFolder))
             {
+               
+              
+                 System.IO.FileSystemWatcher oMultipleFileWatcher;
+                 oMultipleFileWatcher = new FileSystemWatcher();    //the object go into scope now
 
-                 System.Console.WriteLine(sMonitorFolder);
-                 System.IO.FileSystemWatcher oFileWatcher = new FileSystemWatcher(); 
-                 oFileWatcher.Path = sMonitorFolder;  //Set the path that you want to monitor.
-                 oFileWatcher.Filter = "*.log";  //Set the Filter Expression., only watch log files.
+                    //Set the path that you want to monitor. 
+                    oMultipleFileWatcher.Path = sMonitorFolder;
+                    
+                    //Set the Filter Expression., only watch log files.
+                 oMultipleFileWatcher.Filter = "*.*";  
+
+                    // Add event handlers.
+                 oMultipleFileWatcher.Created += Program.FSWatcher_Created;  // |->  method  
+                 oMultipleFileWatcher.Deleted += Program.FSWatcher_Deleted;  // |->  method
+                 oMultipleFileWatcher.Renamed += Program.FSWatcher_Renamed;  // |->  method
+                    
+                    // Begin watching.
+                 oMultipleFileWatcher.EnableRaisingEvents = true;
 
 
-                // Add event handlers.
-                 oFileWatcher.Created += Program.FSWatcher_Created;  // |->  method  
-                 oFileWatcher.Deleted += Program.FSWatcher_Deleted;  // |->  method
-                 oFileWatcher.Renamed += Program.FSWatcher_Renamed;  // |->  method
-                // Begin watching.
-                 oFileWatcher.EnableRaisingEvents = true;
+                    //Creating multiple instances of one object
+                    //Multiple calls to oMultipleFileWatcher() would keep adding objects to the list with this design.
+                    //https://stackoverflow.com/questions/35232565/create-multiple-instances-of-an-object-c-sharp
+                    //https://dzone.com/articles/different-ways-of-creating-list-of-objects-in-c
+                    //Add a new instance  
+                    aFileWatcherInstance.Add(oMultipleFileWatcher);
 
-                //Add a new instance of FileWatcher 
-                 aFileWatcherInstance.Add(oFileWatcher);
+                
+
+                    Console.BackgroundColor = ConsoleColor.DarkGreen;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    System.Console.WriteLine(sMonitorFolder);
+
                 }
 
             }
@@ -86,12 +111,22 @@ namespace MonitorIntelFolderActivity
 
         private static void FSWatcher_Created(object sender, System.IO.FileSystemEventArgs e)
         {
-           // Console.WriteLine("File created: {0}", e.Name);
-         
+            
+            if (e.Name== "Nouveau document texte.txt")
+            {
+                string sLog = "Nouveau document texte !"; AppendText(sLog);
+            }
+              else
+            { // Console.WriteLine("File created: {0}", e.Name);
+                string sLog = "File created: " + e.Name;  AppendText(sLog);}
+          
         }
+
         private static void AppendText(string sLog)
         {
-        Console.WriteLine(Convert.ToString(DateTime.Now) +" - " + sLog);
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(Convert.ToString(DateTime.Now) +" - " + sLog);
         }
     }
 }
