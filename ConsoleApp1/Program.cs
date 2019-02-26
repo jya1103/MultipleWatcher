72541c007538e9b2d686a04093c081163b2025de
jya1103 <47504382+jya1103@users.noreply.github.com>
2019-02-26 22:34:49 +0100

    v6

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,6 +7,7 @@ using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.InteropServices;
+using static System.Net.Mime.MediaTypeNames;
 //https://www.infoworld.com/article/3185447/c-sharp/how-to-work-with-filesystemwatcher-in-c.html
 //https://www.geeksforgeeks.org/c-sharp-arraylist-class/
 namespace MonitorIntelFolderActivity
@@ -111,15 +112,20 @@ string[] args = Environment.GetCommandLineArgs();
             System.Console.WriteLine("..");
             foreach (string sMonitorFolder in lstFolders)
             {
-               
-             
+                //premier argument 
+                System.Console.WriteLine(sMonitorFolder);
+                // on définit une couleur DarkGreen
+                Console.BackgroundColor = ConsoleColor.DarkGray;
+                Console.ForegroundColor = ConsoleColor.White;
+                // le deuxième argument aura de la couleur DarkGray
+
                 if (Directory.Exists(sMonitorFolder))   //Only if Directory Exists
                 {
                     System.IO.FileSystemWatcher oMultipleFileWatcher;  // Create a new FileSystemWatcher and set its properties.
-                    oMultipleFileWatcher = new FileSystemWatcher();   
+                    oMultipleFileWatcher = new FileSystemWatcher();
+                 
 
-       
-                  {  //Set the path that you want to monitor. 
+                    {  //Set the path that you want to monitor. 
                     oMultipleFileWatcher.Path = sMonitorFolder;
                     
                     //Set the Filter Expression., only watch log files.
@@ -134,20 +140,18 @@ string[] args = Environment.GetCommandLineArgs();
                     oMultipleFileWatcher.EnableRaisingEvents = true;
 
 
-                    //Creating multiple instances of one object
-                    //Multiple calls to oMultipleFileWatcher() would keep adding objects to the list with this design.
-                    //https://stackoverflow.com/questions/35232565/create-multiple-instances-of-an-object-c-sharp
-                    //https://dzone.com/articles/different-ways-of-creating-list-of-objects-in-c
-                    //Add a new instance  
-                    aFileWatcherInstance.Add(oMultipleFileWatcher);
+                        //Creating multiple instances of one object
+                        //Multiple calls to oMultipleFileWatcher() would keep adding objects to the list with this design.
+                        //https://stackoverflow.com/questions/35232565/create-multiple-instances-of-an-object-c-sharp
+                        //https://dzone.com/articles/different-ways-of-creating-list-of-objects-in-c
+                     
+                        //Add a new instance  
+                        aFileWatcherInstance.Add(oMultipleFileWatcher);
+                     
 
-                
 
-                    Console.BackgroundColor = ConsoleColor.DarkGreen;
-                    Console.ForegroundColor = ConsoleColor.White;
-                    System.Console.WriteLine(sMonitorFolder);
-                      
-                   }
+
+                    }
                 }
             }
            
@@ -190,8 +194,9 @@ string[] args = Environment.GetCommandLineArgs();
             Console.BackgroundColor = ConsoleColor.DarkGray;
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine(Convert.ToString(DateTime.Now) +" - " + sLog);
-            MessageBox((IntPtr)0, (Convert.ToString(DateTime.Now) + " - " + sLog), "My Message Box", 0);
-
+          
         }
+
+      
     }
 }
